add3dbc99a91199bef4784f187606513a0e0bc75
Victor Ngigi Njoroge <vnjorogen2001@gmail.com>
2023-06-25 13:21:47 +0300

    Completed Assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+    CATEGORY = ['Fiction', 'Non-Fiction']
+    validates :title, presence: true
+    validates :category, inclusion: {in: CATEGORY}
+    validates :content, length: {minimum: 100}
+
 end
